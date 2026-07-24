1ff0cc55eb17438ace583b0f1eb66c75e5113e0c
Netsanet Demis <209745032+netsy-d@users.noreply.github.com>
2026-07-24 12:31:03 +0300

    Update app/api/search/route.ts
    Co-authored-by: Dires Aman <79739965+diresaman@users.noreply.github.com>

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -3,7 +3,7 @@ import { searchMovies } from "@/lib/omdb";
 import { isOMDbError } from "@/types/movie";
 
 
-export async function Get(request:NextRequest) {
+export async function GET(request:NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const query = searchParams.get("q")?.trim();
     const page = Number(searchParams.get("page") ?? "1");
